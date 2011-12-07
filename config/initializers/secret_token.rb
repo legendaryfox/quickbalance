457847e6c0b7091ec191f5a75b3b10d04b7e2745
legendaryfox <legendaryfox@gmail.com>
2011-12-07 15:00:33 +0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quickbalance::Application.config.secret_token = '38e83644a6291e4016e96e5916113ae2350f32a7d4c0e4e6c147a6e0095d08718b13490c832ceb15ce318a05f95cdbcc8a39fe808919037b8c8bb503a671030d'
